1766210c6b626a148da3c4bd6a5d10d39f431bf8
Andriy Hafyak <x3x7x3@gmai.com>
2016-10-29 16:19:26 +0200

    init commit

---
+++ src/Test.java
@@ -0,0 +1,16 @@
+/**
+ * Created by user on 27.10.2016.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println("Test");
+        System.out.println("Test");
+        System.out.println("Test");
+
+    }
+
+
+
+}
